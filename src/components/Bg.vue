a736dab24cb6ef0567d317b83e30acb1346d0aa0
Nikita Beliaev <Nikirozzz@gmail.com>
2025-10-04 15:28:27 +0200

    threejs background

--- src/components/Bg.vue
+++ src/components/Bg.vue
@@ -8,22 +8,183 @@ import {
     WebGLRenderer,
     Scene,
     PerspectiveCamera,
-    BoxGeometry,
     MeshLambertMaterial,
     PlaneGeometry,
     Mesh,
-    DirectionalLight,
+    AmbientLight,
     Color,
     TextureLoader,
-    Clock
+    Clock,
+    Vector3
 } from 'three'
+import Stats from 'stats.js'
+
+const stats = new Stats()
+stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild(stats.dom)
 
 const wrap = ref(null)
 
-let renderer, scene, smokeParticles, smokeGeo, smokeMaterial, light, smokeTexture, camera, geometry, material, rafId, mesh, cubeSineDriver
-let handleResize
-let delta = 0;
+let renderer, scene, smokeBgParticles,
+    light, camera, rafId,
+    sparkParticles, smokeFgParticles
+
+let handleResize;
+let spawnTimer = 0;
+const spawnInterval = 0.1;
+const maxParticles = 40;
+
 const clock = new Clock();
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+function lerp(a, b, f) {
+    return a * (1 - f) + b * f
+
+}
+
+
+class SparkParticle {
+    constructor(scene) {
+        this.prevPos = new Vector3();
+        var rnd = Math.random() * 1 - 3;
+        const geometry = new PlaneGeometry(rnd, rnd / 2);
+        const textureLoader = new TextureLoader();
+
+        const texture = textureLoader.load(
+            "./Circle.png"
+        );
+
+        texture.center.set(0.5, 0.5);
+        this.material = new MeshLambertMaterial({
+            color: new Color("#b2e5fb"),
+            map: texture,
+            transparent: true
+        });
+
+        this.mesh = new Mesh(geometry, this.material);
+        scene.add(this.mesh);
+        this.reset();
+    }
+
+    reset() {
+        this.baseX = getRandomArbitrary(-65, -25);
+        this.yPos = getRandomArbitrary(-40, -35);
+        this.speed = getRandomArbitrary(15, 30);
+        this.freq1 = getRandomArbitrary(1, 3);
+        this.freq2 = getRandomArbitrary(2, 4);
+        this.amplitude1 = getRandomArbitrary(1, 7);
+        this.amplitude2 = getRandomArbitrary(2, 9);
+        this.offset1 = getRandomArbitrary(Math.PI, Math.PI * 25);
+        this.offset2 = getRandomArbitrary(Math.PI, Math.PI * 45);
+        this.opacity = getRandomArbitrary(0.5, 0.9);
+        this.opacitySpeed = getRandomArbitrary(0.2, 0.4);
+        this.deadEnd = getRandomArbitrary(35, 50)
+    }
+
+    update(delta, elapsed) {
+
+        this.yPos += this.speed * delta;
+        this.mesh.position.y = this.yPos;
+        this.opacity -= this.opacitySpeed * delta;;
+        this.material.opacity = this.opacity;
+
+        this.mesh.position.x =
+            this.baseX +
+            Math.sin(elapsed * this.freq1 + this.offset1) * this.amplitude1 +
+            Math.sin(elapsed * this.freq2 + this.offset2) * this.amplitude2;
+
+        this.mesh.position.z = 950;
+
+        let velocity = new Vector3().subVectors(this.mesh.position, this.prevPos);
+
+        if (velocity.lengthSq() > 0.0001) {
+            const angle = Math.atan2(velocity.y, velocity.x);
+            this.mesh.rotation.z = angle;
+        }
+
+        this.prevPos.copy(this.mesh.position);
+
+        if (this.yPos > this.deadEnd) {
+            this.yPos = -this.deadEnd;
+            this.reset();
+        }
+
+    }
+}
+
+class SmokeParticle {
+    constructor(scene, size, color, x, y, z, tex = "any") {
+
+        const textureLoader = new TextureLoader()
+        if (tex == "any") {
+            if (Math.random() > 0.5) {
+                this.smokeTexture = textureLoader.load(
+                    "./Smoke.png"
+                );
+            } else {
+                this.smokeTexture = textureLoader.load(
+                    "./Smoke2.png"
+                );
+            }
+        } else {
+            this.smokeTexture = textureLoader.load(tex);
+        }
+
+        this.smokeTexture.center.set(0.5, 0.5);
+
+        this.material = new MeshLambertMaterial({
+            color: new Color(color),
+            map: this.smokeTexture,
+            transparent: true
+        });
+
+        this.smokeGeo = new PlaneGeometry(size, size);
+
+        this.mesh = new Mesh(this.smokeGeo, this.material);
+        this.mesh.position.set(
+            x,
+            y,
+            z
+        );
+
+        this.mesh.rotation.z = Math.random() * 360;
+
+        scene.add(this.mesh);
+    }
+
+    update(delta) {
+        this.mesh.rotation.z += delta * 0.2;
+    }
+
+}
+
+class SmokeParticleBlink extends SmokeParticle {
+    constructor(scene, size, color, x, y, z) {
+        super(scene, size, color, x, y, z,"./Smoke.png");
+
+        this.minOpacity = 0.3;
+        this.maxOpacity = 0.8;
+        this.blinkSpeed = 1 + Math.random();
+        this.phase = Math.random() * Math.PI * 2;
+        this.isMaxVisible = false;
+        this.lerpSpeed = 5;
+        this.currentOpacity = 1;
+    }
+
+    setForceVisible(on = true) {
+        this.isMaxVisible = on;
+    }
+
+    update(delta) {
+        super.update(delta);
+        const k = 1 - Math.exp(-this.lerpSpeed * delta);
+        var target = this.isMaxVisible ? this.maxOpacity : this.minOpacity;
+        this.currentOpacity = lerp(this.currentOpacity, target, k);
+        this.mesh.material.opacity = this.currentOpacity;
+    }
+}
 
 onMounted(() => {
 
@@ -37,7 +198,7 @@ onMounted(() => {
         wrap.value.appendChild(renderer.domElement);
 
         scene = new Scene();
-        scene.background = new Color("#020f1c");
+        scene.background = new Color("#001525");
 
         camera = new PerspectiveCamera(
             75,
@@ -45,47 +206,45 @@ onMounted(() => {
             1,
             10000
         );
+
         camera.position.z = 1000;
         scene.add(camera);
 
-        geometry = new BoxGeometry(200, 200, 200);
-        material = new MeshLambertMaterial({
-            color: new Color("#17202d"),
-            wireframe: false
-        });
-
-        mesh = new Mesh(geometry, material);
-        cubeSineDriver = 0;
-
-        light = new DirectionalLight(0xffffff, 1);
-        light.position.set(-1, 0, 1);
+        light = new AmbientLight(0xffffff, 1);
         scene.add(light);
-        const textureLoader = new TextureLoader()
-        smokeTexture = textureLoader.load(
-            "./Smoke.png"
-        );
-        smokeTexture.center.set(0.5, 0.5);
 
-        smokeMaterial = new MeshLambertMaterial({
-            color: new Color("#17202d"),
-            map: smokeTexture,
-            transparent: true
-        });
-        smokeGeo = new PlaneGeometry(300, 300);
-        smokeParticles = [];
+        smokeBgParticles = [];
 
-        for (let p = 0; p < 150; p++) {
-            var particle = new Mesh(smokeGeo, smokeMaterial);
-            particle.position.set(
+        for (let i = 0; i < 150; i++) {
+            smokeBgParticles.push(new SmokeParticle(
+                scene,
+                300,
+                "#002642",
                 Math.random() * 500 - 250,
                 Math.random() * 500 - 250,
                 Math.random() * 1000 - 100
-            );
-            particle.rotation.z = Math.random() * 360;
-            scene.add(particle);
-            smokeParticles.push(particle);
+            ));
         }
 
+        smokeFgParticles = [];
+
+        var xOffset = -15;
+        var yOffset = -2.5;
+
+        for (let i = 0; i < 5; i++) {
+            smokeFgParticles.push(new SmokeParticleBlink(
+                scene,
+                20,
+                "#77b0d6",
+                xOffset,
+                yOffset,
+                990
+            ));
+            xOffset += 1.5;
+            yOffset -= 1.7;
+        }
+
+        sparkParticles = [];
     }
 
     handleResize = () => {
@@ -95,42 +254,50 @@ onMounted(() => {
         camera.aspect = newW / newH
         camera.updateProjectionMatrix()
     }
-    window.addEventListener('resize', handleResize)
 
     const tick = () => {
-        delta = clock.getDelta()
+        stats.begin()
         rafId = requestAnimationFrame(tick)
-        evolveSmoke();
-        render();
-    }
 
-    function evolveSmoke() {
-        var sp = smokeParticles.length;
-        while (sp--) {
-            smokeParticles[sp].rotation.z += delta * 0.2;
+        var delta = clock.getDelta();
+        const elapsed = clock.elapsedTime;
+
+        spawnTimer += delta;
+        if (spawnTimer >= spawnInterval && sparkParticles.length < maxParticles) {
+            sparkParticles.push(new SparkParticle(scene));
+            spawnTimer = 0;
         }
+
+        smokeBgParticles.forEach(p => p.update(delta));
+        smokeFgParticles.forEach(p => p.update(delta));
+        sparkParticles.forEach(p => p.update(delta, elapsed));
+
+        render();
+        stats.end()
     }
 
     function render() {
         renderer.setClearColor(0xffffff, 0);
-        mesh.rotation.x += 0.005;
-        mesh.rotation.y += 0.01;
-        cubeSineDriver += 0.01;
-        mesh.position.z = 100 + Math.sin(cubeSineDriver) * 500;
         renderer.render(scene, camera);
     }
 
+    function click() {
+        smokeFgParticles.forEach(p => p.setForceVisible(!p.isMaxVisible));
+    }
+
     init();
+    handleResize();
     tick();
+
+    window.addEventListener('resize', handleResize);
+    //window.addEventListener('click', click);
 
 })
 
 onUnmounted(() => {
     cancelAnimationFrame(rafId)
-    window.removeEventListener('resize', handleResize)
-    renderer?.dispose()
-    geo?.dispose()
-    mat?.dispose()
+    window.removeEventListener('resize', handleResize);
+    renderer?.dispose();
 })
 </script>
 
