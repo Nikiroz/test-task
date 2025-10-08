8000634e9de3e323f8153bb10c83ed39ceed90f8
Nikita Beliaev <Nikirozzz@gmail.com>
2025-10-08 15:02:54 +0200

    rebuild to docker

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
+import { fileURLToPath } from 'node:url'
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+const rootDir = fileURLToPath(new URL('.', import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/test-task/',
+  base: '/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(rootDir, 'index.html'),
+        mobile: resolve(rootDir, 'mobile.html')
+      }
+    }
   }
 })
