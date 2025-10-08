44c2730728146127ef7036a3e040f43f3bf4e845
Nikita Beliaev <Nikirozzz@gmail.com>
2025-10-08 12:33:06 +0200

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.GITHUB_REPOSITORY?.endsWith('.github.io') ? '/' : '/test-task/',
+  build: {
+    outDir: 'dist'
+  }
 })
